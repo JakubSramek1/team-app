41a5bb33f3493a17e42f0cf1f8340b3ceef36398
Jakub Šrámek <sramekvm@gmail.com>
2023-06-27 17:35:08 +0200

    connect supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import './App.css';
+import { createClient } from "@supabase/supabase-js";
+import { useEffect } from "react";
+
 
 function App() {
+  const supabaseKey = process.env.REACT_APP_SUPABASE_KEY || ''
+ useEffect(() => {
+  handler()
+ }, [])
+
+   const supabase = createClient(
+    "https://nktebdhspzvpwguqcksn.supabase.co/rest/v1/teams?select=*",
+    supabaseKey
+  );
+
+  const handler = async () => {
+    const { data, error } = await supabase
+  .from('countries')
+  .select()
+  console.log(data)
+  }
+
   return (
     <div className="App">
-     
+     ewfgdsg
     </div>
   );
 }
